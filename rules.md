660a9d27da7404ce9240df62eadd25023c67f3f9
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2025-04-06 23:06:13 +0200

    Initial commit

---
+++ rules.md
@@ -0,0 +1,91 @@
+---
+up:
+  - "[[통사론]]"
+tags: []
+icon: 
+created: 2025-02-14 21:29
+updated: 2025-04-05 19:51
+---
+
+# Suggestions
+
+- KMS2: Chapter 12
+
+## Let's...
+
+### [[Casual speech]]
+
+> [!rule] VS + 자
+>
+> > [!예]
+> > 가자
+>
+> > [!예문-문장]
+> > 집에 가자.
+> > Let's go home.
+
+### [[5 해요체]] ([[Informal speech]])
+
+> [!rule] 같이 + 요(V)
+> let's do V together
+>
+> > [!예문-문장]
+> > 같이 연습해요.
+> > Let's practice together.
+
+### [[Formal speech]]
+
+> [!rule] VS + (으)시죠
+>
+> > [!info]
+> > Use this when talking to your boss.
+> >
+> > Technically a command, but due to the honorific ending 죠 it becomes a suggestion.
+>
+> > [!예]
+> > 일하시죠
+
+> [!rule] VS + (으)시다
+> [[높임말|H]] let's...
+>
+> > [!예문-문장]
+> > 같이 일하시다?
+> > Shall we work together?
+
+### [[Informal speech]]
+
+> [!rule] VS + (으)ㅂ시다
+> [[높임말|H]] Let's do V.
+>
+> > [!예]
+> > 가다 -> 갑시다
+>
+> > [!info]
+> > Used by higher ranking people when talking to lower ranking people.
+>
+> > [!warning]
+> > This form is not polite to use with anyone that you wouldn't talk to informally. It also sounds awkward in [[Casual speech]].
+>
+> > [!tip]
+> > It's better to avoid using this form.
+>
+> > [!예문-문장]
+> > 파티에 갑시다.
+> > Let's go to the party.
+>
+
+## Shall we...
+
+> [!rule] VS + (으)ㄹ까요
+> shall we V?
+>
+> > [!예문-문장]
+> > 제가 누구에게 먼저 전화할까요?
+> > Who should I call first?
+
+> [!rule] VS + 실까요
+> [[높임말|H]] shall we V?
+>
+> > [!예문-문장]
+> > 뭘 만들실까요?
+> > What shall we make?
---
+++ src/main.rs
@@ -0,0 +1,61 @@
+mod callout;
+use callout::Callout;
+use jwalk::WalkDir;
+use rayon::prelude::*;
+use std::{borrow::BorrowMut, env, error::Error, fs::read_to_string, io, path::Path};
+
+// #[macro_use]
+// extern crate hyperscan;
+
+fn process_file(path: &Path) -> Result<Vec<Callout>, Box<dyn Error + Send + Sync>> {
+    // let c =
+    let content = read_to_string(path)?;
+    // TODO: single thread: split file into callout blocks
+    let binding: Vec<_> = content
+        .split("\n> [!")
+        .skip(1)
+        .filter(|block| !block.trim().is_empty())
+        .map(|block| format!("> [!{}", block))
+        .collect::<Vec<String>>();
+
+    // TODO: parallel: parse all callout blocks into a callout structs and convert the structs into
+    // anki card text
+    let vals: Vec<_> = binding
+        .par_iter()
+        .map(|block| {
+            block
+                .par_split('\n')
+                .filter(|line| line.starts_with('>'))
+                .collect::<Vec<_>>()
+        })
+        .map(|block| Callout::try_from(block))
+        .collect();
+    dbg!(vals);
+
+    // TODO: merge callouts text into single string
+    Ok(Vec::new())
+}
+
+fn main() -> io::Result<()> {
+    let current_dir = env::current_dir()?;
+    dbg!(&current_dir);
+    // let args: Vec<String> = env::args().collect();
+    // dbg!(args);
+    // let target_dir = "/mnt/c/Users/Lev/Obsidian/Vault/Languages/한국어";
+    let target_dir = current_dir;
+    // dbg!(target_dir);
+    let markdown_files: Vec<_> = WalkDir::new(target_dir)
+        .into_iter()
+        .map(|entry| entry.unwrap().path())
+        .filter(|path| path.extension().is_some_and(|ext| ext == "md"))
+        .collect();
+    dbg!(&markdown_files);
+    markdown_files
+        .par_iter()
+        .map(|path| process_file(path))
+        .collect::<Vec<_>>();
+
+    // TODO: stream processed text into file
+
+    Ok(())
+}

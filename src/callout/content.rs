1a53028eea05c6a28c5520bfdae8700288f2d285
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2025-04-21 11:18:25 +0200

    refactor: remove uneeded method from CalloutContent

--- src/callout/content.rs
+++ src/callout/content.rs
@@ -13,20 +13,3 @@ pub enum CalloutContent {
     Blockquote(String),
     HorizontalLine,
 }
-
-impl CalloutContent {
-    pub fn to_html(&self) -> String {
-        match &self {
-            CalloutContent::Text(value) => value.to_string(),
-            CalloutContent::UnorderedListItem(value) => value.to_string(),
-            CalloutContent::OrderedListItem(_) => todo!(),
-            CalloutContent::SubCalloutIndex(_) => todo!(),
-            CalloutContent::Blockquote(_) => todo!(),
-            CalloutContent::HorizontalLine => todo!(),
-            CalloutContent::UnorderedListStart => todo!(),
-            CalloutContent::UnorderedListEnd => todo!(),
-            CalloutContent::OrderedListStart => todo!(),
-            CalloutContent::OrderedListEnd => todo!(),
-        }
-    }
-}

7a24bc354d6bde74e5d77a5da94029c48ca34899
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2025-04-24 23:22:08 +0200

    refactor: update model traits

--- src/model/basic.rs
+++ src/model/basic.rs
@@ -2,34 +2,12 @@ use ankiconnect_rs::Field;
 
 use crate::{callout::Callout, utils::capitalize_first_letter};
 
-use super::traits::{AddNote, CreateModel, FromCallout, InternalModel};
+use super::traits::{CreateModel, InternalModel};
 
 #[derive(Debug)]
 pub struct Basic {
     pub front: String,
     pub back: String,
-}
-
-impl FromCallout for Basic {
-    fn from_callout(callout: &Callout, header_lang: Option<&str>) -> Self {
-        Basic {
-            front: callout.header.clone(),
-            back: callout.content_to_html(header_lang),
-        }
-    }
-}
-
-impl AddNote for Basic {
-    fn add_note(
-        &self,
-        deck_name: &ankiconnect_rs::Deck,
-        model: &ankiconnect_rs::Model,
-        note: ankiconnect_rs::Note,
-        allow_duplicate: bool,
-        duplicate_scope: Option<ankiconnect_rs::DuplicateScope>,
-    ) -> ankiconnect_rs::Result<ankiconnect_rs::NoteId> {
-        todo!()
-    }
 }
 
 impl CreateModel for Basic {
@@ -89,6 +67,15 @@ impl CreateModel for Basic {
     }
 }
 
+impl InternalModel for Basic {
+    fn from_callout(callout: &Callout, header_lang: Option<&str>) -> Self {
+        Basic {
+            front: callout.header.clone(),
+            back: callout.content_to_html(header_lang),
+        }
+    }
+}
+
 impl Default for Basic {
     fn default() -> Self {
         Basic {
@@ -97,5 +84,3 @@ impl Default for Basic {
         }
     }
 }
-
-impl InternalModel for Basic {}

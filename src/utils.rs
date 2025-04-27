60f7c400f535229f408e16ecbe2e97773b9c6cc4
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2025-04-27 23:08:08 +0200

    Remove undeed module

--- src/utils.rs
+++
@@ -1,8 +0,0 @@
-pub fn capitalize_first_letter(text: &str) -> String {
-    let mut chars = text.chars();
-    match chars.next() {
-        None => String::new(),
-        Some(c) => c.to_uppercase().collect::<String>() + chars.as_str(),
-    }
-}
-

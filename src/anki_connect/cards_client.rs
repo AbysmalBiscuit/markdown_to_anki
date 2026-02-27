a47cc38c512a27238f56ac3da1a3d728cd32cff3
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2026-02-27 17:57:28 +0100

    feat: implement new find_cards function for cards client and add docs

--- src/anki_connect/cards_client.rs
+++ src/anki_connect/cards_client.rs
@@ -1,9 +1,21 @@
-use super::AnkiConnectClient;
+use super::{
+    AnkiConnectClient, card::CardId, client::ClientBehavior, error::APIError, response::Response,
+};
 
+/// Card actions.
+#[allow(dead_code)]
 #[derive(Debug, Clone)]
 pub struct CardsClient<'a>(pub &'a AnkiConnectClient);
 
-impl CardsClient<'_> {}
+impl CardsClient<'_> {
+    /// Returns an array of card IDs for a given query. Functionally identical to guiBrowse but doesn't use the GUI for better performance.
+    pub fn find_cards(&self, query: &str) -> Result<Vec<CardId>, APIError> {
+        let response: Response<Vec<CardId>> = self
+            .0
+            .request("findCards", Some(params::FindCards::new(query)))?;
+        Ok(response.result.unwrap_or_default())
+    }
+}
 
 pub mod params {
     use derive_new::new;
@@ -11,7 +23,7 @@ pub mod params {
 
     #[derive(Debug, Serialize, new)]
     #[serde(rename_all = "camelCase")]
-    pub struct ActionPrams {
-        var: String,
+    pub struct FindCards<'a> {
+        query: &'a str,
     }
 }
